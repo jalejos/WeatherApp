2cecc1274bfd852c5da9d36630dbe3795f506959
jalejos <jalejos@nearsoft.com>
2017-03-20 23:05:01 -0700

    Fix typo on celsius url parameter declaration

--- WeatherApp/WeatherApp/WeatherRepository.swift
+++ WeatherApp/WeatherApp/WeatherRepository.swift
@@ -11,10 +11,10 @@ import Alamofire
 
 struct WeatherRepository {
     static let appId = "3f3a608541a999f9d309a7f2b3f36ac7"
-    static let celciusUnits = "metric"
+    static let celsiusUnits = "metric"
     
     static func getWeather (geolocation: Geolocation, onClosure: @escaping (_ weather: Dictionary <String, Any>?, _ error: Error?) -> Void) {
-        Alamofire.request("http://api.openweathermap.org/data/2.5/weather?lat=\(geolocation.latitude)&lon=\(geolocation.longitude)&appid=\(appId)&units=\(celciusUnits)").responseJSON(completionHandler: { (response) in
+        Alamofire.request("http://api.openweathermap.org/data/2.5/weather?lat=\(geolocation.latitude)&lon=\(geolocation.longitude)&appid=\(appId)&units=\(celsiusUnits)").responseJSON(completionHandler: { (response) in
             if let responseError = response.error {
                 onClosure(nil, responseError)
             }
